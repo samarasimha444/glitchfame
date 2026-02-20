71e626d3440056c21f0806d3fc7ab8259f354363
samarasimhareddy <samarasimha444@gmail.com>
2026-02-20 15:46:09 +0530

    intialized the folders

---
+++ backend/src/main/java/com/example/glitchfame/GlitchfameApplication.java
@@ -0,0 +1,13 @@
+package com.example.glitchfame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlitchfameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlitchfameApplication.class, args);
+	}
+
+}
