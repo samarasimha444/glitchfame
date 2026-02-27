855381095587351a54cad85c3bc8f342ca146106
samarasimhareddy <samarasimha444@gmail.com>
2026-02-27 12:10:57 +0530

    fix: resolve routing structure and correct App.jsx syntax

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,10 +9,11 @@ import RoleRedirect from "./Auth/RoleRedirect";
 import EntryForm from "./Roles/Users/EntryForm";
 import Arena from "./Roles/Users/Arena";
 import PlayerDetail from "./Roles/Users/PlayerDetails";
+import Testing from "./Testing";
+
 export default function App() {
   return (
     <Routes>
-     
 
       {/* Root role-based redirect */}
       <Route path="/" element={<RoleRedirect />} />
@@ -20,50 +21,17 @@ export default function App() {
       {/* Public Routes */}
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/Testing" element={<Testing/>} />
-      
-
-
-
+      <Route path="/testing" element={<Testing />} />
 
       {/* USER Protected Routes */}
       {/* <Route element={<ProtectedRoute allowedRole="USER" />}> */}
         <Route element={<Layout />}>
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/enter" element={<EntryForm/>} />
-           <Route path="/arena" element={<Arena/>} />
-            <Route path="/details" element={<PlayerDetail/>} />
-            
-            
-          
+          <Route path="/enter" element={<EntryForm />} />
+          <Route path="/arena" element={<Arena />} />
+          <Route path="/details" element={<PlayerDetail />} />
         </Route>
-        
       {/* </Route> */}
-
-
-
-    {/* ADMIN Protected Routes */}
-
-
-
- 
-
-
-  {/* <Route element={<Layout />}>
-   
-
-      <Route path="seasons" element={<Seasons />} />
-
-      <Route path="seasons/create" element={<SeasonForm />} />
-      <Route path="seasons/:id" element={<SeasonDetails />} />
-
-     
-    </Route>
-  </Route> */}
-</Route>
-
-
- 
 
     </Routes>
   );
