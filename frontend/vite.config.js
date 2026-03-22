ffb41fe83f0f2b4d9202110f4896f0c264ed741b
preet0006 <ps743298@gmail.com>
2026-03-22 08:21:12 +0530

    fix config vote

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,47 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { analyzer } from "vite-bundle-analyzer";
+import path from "path";
 
 export default defineConfig({
+  mode: "production",
   plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
+    react(), // remove babel-plugin-react-compiler
     tailwindcss(),
+    analyzer({
+      open: true,
+      summaryOnly: false,
+      gzipSize: true,
+      brotliSize: true,
+    }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    sourcemap: false,
+    minify: "esbuild",
+    cssMinify: true,
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react-router-dom")) return "router";
+            if (id.includes("@tanstack/react-query")) return "query";
+            if (id.includes("lucide-react")) return "icons";
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ["react", "react-dom", "@tanstack/react-query"],
+  },
 });
